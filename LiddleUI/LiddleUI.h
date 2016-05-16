cfcd13be65bec58da39c99506d98ac87c424c2bf
Peter Liddle <ptliddle@gmail.com>
2016-05-16 11:33:25 -0700

    Initial Commit

---
+++ LiddleUI/LiddleUI.h
@@ -0,0 +1,19 @@
+//
+//  LiddleUI.h
+//  LiddleUI
+//
+//  Created by Peter Liddle on 5/16/16.
+//  Copyright © 2016 Peter Liddle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LiddleUI.
+FOUNDATION_EXPORT double LiddleUIVersionNumber;
+
+//! Project version string for LiddleUI.
+FOUNDATION_EXPORT const unsigned char LiddleUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LiddleUI/PublicHeader.h>
+
+
